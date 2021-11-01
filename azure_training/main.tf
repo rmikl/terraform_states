71bd91575309d889c6add18ee8a1530d68b2995d
rmikl <robert.miklas@protonmail.com>
2021-11-01 18:25:30 +0100

    deleted resources

--- azure_training/main.tf
+++ azure_training/main.tf
@@ -10,13 +10,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-resource "azurerm_resource_group" "example" {
-  name     = "production-resources-tf"
-  location = "northeurope"
-}
-
-resource "azurerm_resource_group" "example2" {
-  name     = "production-resources-tf2"
-  location = "northeurope"
-}
