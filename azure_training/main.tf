dc0579ab0d6b63fb419fac0bd24060c19e0949cd
rmikl <robert.miklas@protonmail.com>
2021-11-01 10:50:04 +0100

    fix 109

--- azure_training/main.tf
+++ azure_training/main.tf
@@ -9,12 +9,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "44ddbd0f-22a4-4dd1-9c1c-c72e61a3fb69"
-  tenant_id       = "cf17c4a9-7128-4bf9-ad02-8f9114ecaf5c"
-}
 
-
-resource "azurerm_resource_group" "example" {
-  name     = "production-resources-tf"
-  location = "northeurope"
 }
