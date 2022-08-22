570b115e2a3d2b9b0f7e11e172e15026643adb05
rmikl <robert.miklas@protonmail.com>
2022-08-22 09:30:41 +0200

    fix in ec2-3 ssecurity groups added to comunicate with aws

--- providers/aws/ec2-3/modules/ec2-deploy/security.tf
+++ providers/aws/ec2-3/modules/ec2-deploy/security.tf
@@ -10,14 +10,6 @@ resource "aws_security_group" "traffic_to_jump_host" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   tags = {
     name = "allow_ssh"
   }
@@ -40,6 +32,25 @@ resource "aws_security_group" "traffic_to_infra" {
   }
 }
 
+resource "aws_security_group" "traffic_outbound_full" {
+  name        = "traffic_outbound_full"
+  description = "allow all outboumd traffic"
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    name = "allow_outbound"
+  }
+}
+
+
 resource "tls_private_key" "private_key_jump"{
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -60,3 +71,55 @@ resource "aws_key_pair" "pub_key_jump" {
   key_name   = "pub_key_jump"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIxAN5kuLqNfJMqMtbbLKJqV2eJgPNi+ai4uQ1BHtrWQsm7y72FyqsWfUk7vKdd0nmC+0GNflVUlJzK2Hp5oYNIhxCme38joNOMAQAzFk22KJZgsVYbtJOERySLLl9LsZQC+dmVjjIMZs8SNmM0Brw0xXExzVRbyyFDLZk5J+g18y1DprzU/S6E4iLoH/S63Bf37oR+pqJz16X4DBG0WVTiCPYj9vT6lq0Ljt6rOzrF+03cvI7mzNmMappi8X49TrwVUuBTTWBgf9k/Sir1SquhQO3vB8k9jASiyeBl0ON+/N3RaJ7C7oTWCRpEhck80ODfPOvmoyONtnpDjegAfVDHCQNeEo/n4Z77zxu4r6JbZXs4/ZjE1Q1ieXpKTQy7ESSbsJUCB6uUVpECyGWoMa/s8QIh8DlRdwtEiyeAZY4ZuWQlnt0eSj7jdTKJTY9rMAvw7yyzxJfT3p34FJ/MUqrnFnojQs9VQbhQyVwvwrp9GCSsssWH9rtNxvtDI5YgiU="
 }
+
+resource "aws_iam_role" "iam_role_for_infra" {
+  name = "iam_role_for_infra"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "iam_instance_profile_infra" {
+  name = "iam_instance_profile_infra"
+  role = "${aws_iam_role.iam_role_for_infra.name}"
+}
+
+resource "aws_iam_role_policy" "access_to_iam" {
+  name = "access_to_ec2"
+  role = "${aws_iam_role.iam_role_for_infra.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:GenerateCredentialReport",
+                "iam:GenerateServiceLastAccessedDetails",
+                "iam:Get*",
+                "iam:List*",
+                "iam:SimulateCustomPolicy",
+                "iam:SimulatePrincipalPolicy"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+
