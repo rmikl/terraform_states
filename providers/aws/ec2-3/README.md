cd920209bf12a685b878779165513a2d8413f16e
Robert Miklas <robert.miklas@protonmail.com>
2022-08-22 21:03:29 +0200

    fix in ec2-3 ssecurity groups added to comunicate with aws (#14)

--- providers/aws/ec2-3/README.md
+++ providers/aws/ec2-3/README.md
@@ -3,9 +3,9 @@
 there will be open ssh connectivity from jump to prod intance, and also connectivity from outside to jump
 instances: 
 * jump
-* prod
+* infra
 
-from prod there will be possbility to use aws cli, with usage of IAM roles, role should be list ec2 instance,
+from infra host there will be possbility to use aws cli, with usage of IAM roles, it will allow to list ec2 instance
 
 
 in this scenario i will use modules
--- providers/aws/ec2-3/modules/ec2-deploy/ec2.tf
+++ providers/aws/ec2-3/modules/ec2-deploy/ec2.tf
@@ -3,31 +3,30 @@ resource "aws_instance" "jump_host" {
   instance_type = var.aws_instance
   user_data = var.input_script_jump_host
   key_name = aws_key_pair.pub_key_jump.id
-  vpc_security_group_ids = [ aws_security_group.traffic_to_jump_host.id ]
+  vpc_security_group_ids = [ aws_security_group.traffic_to_jump_host.id, aws_security_group.traffic_outbound_full.id ]
+
+
+connection {
+  type        = "ssh"
+  user        = "ec2-user"
+  private_key = "${file("~/.ssh/id_rsa")}"
+  host        = "${self.public_dns}"
+}
 
 provisioner "remote-exec" {
     inline = [
-        "mkdir -p /.ssh"
+        "mkdir -p ~/.ssh"
     ]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = "${file("~/.ssh/id_rsa")}"
-      host        = "${self.public_dns}"
-    }
-  }
-
+}
 provisioner "file" {
     source      = "generated_files/id_rsa_local_machine"
-    destination = "~/.ssh/id_rsa"
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = "${file("~/.ssh/id_rsa")}"
-      host        = "${self.public_dns}"
-    }
+    destination = "/home/ec2-user/.ssh/id_rsa"
   }
+provisioner "remote-exec" {
+    inline = [
+        "chmod 600 ~/.ssh/id_rsa"
+    ]
+}
 
   tags = {
     module_used = "true"
@@ -44,8 +43,8 @@ resource "aws_instance" "infra" {
   instance_type = var.aws_instance
   user_data = var.input_script_infra
   key_name = aws_key_pair.pub_key_infra.id
-  vpc_security_group_ids = [ aws_security_group.traffic_to_infra.id ]
-  
+  vpc_security_group_ids = [ aws_security_group.traffic_to_infra.id, aws_security_group.traffic_outbound_full.id ]
+  iam_instance_profile = "${aws_iam_instance_profile.iam_instance_profile_infra.name}"
   tags = {
     module_used = "true"
     terraform_instance = "true"
