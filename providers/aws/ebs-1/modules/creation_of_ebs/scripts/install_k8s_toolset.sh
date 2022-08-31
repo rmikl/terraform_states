8150cbcd1d891ef427c707be719ae0f583ffafef
rmikl <robert.miklas@protonmail.com>
2022-08-31 22:34:32 +0200

    wip

--- providers/aws/ebs-1/modules/creation_of_ebs/scripts/install_k8s_toolset.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-mkdir -p /media/ebs/
-mount /dev/sdh /media/ebs 
-
-echo "hello from ebs" > /media/ebs/proof
--- providers/aws/ebs-1/modules/creation_of_ebs/vars.tf
+++ providers/aws/ebs-1/modules/creation_of_ebs/vars.tf
@@ -8,3 +8,9 @@ variable "aws_instance" {
     type = string
     default = "t2.micro"
 }
+
+variable "ec2_ami" {
+  type        = string
+  default     = "ami-0c956e207f9d113d5"
+  description = "default ami is ami of Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type"
+}
