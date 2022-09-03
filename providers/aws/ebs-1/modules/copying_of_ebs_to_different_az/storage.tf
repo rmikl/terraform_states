156bbbb33dd63ec5e168bd5ab9bd16ba95b6744e
Robert Miklas <robert.miklas@protonmail.com>
2022-09-03 02:19:07 +0200

    added kms encryption to second module of ebs-1 (#20)

--- providers/aws/ebs-1/modules/copying_of_ebs_to_different_az/storage.tf
+++ providers/aws/ebs-1/modules/copying_of_ebs_to_different_az/storage.tf
@@ -3,10 +3,16 @@ resource "aws_ebs_snapshot" "snapshot_from_volume" {
   depends_on = [ aws_instance.target ] 
 }
 
+data "aws_kms_alias" "kms_key"{
+  name = "alias/aws/ebs"
+}
+
 resource "aws_ebs_volume" "dest" {
   availability_zone = var.dest_az
   size              = 2
   snapshot_id = aws_ebs_snapshot.snapshot_from_volume.id
+  kms_key_id        = data.aws_kms_alias.kms_key.id
+  encrypted = true
 }
 
 resource "aws_volume_attachment" "dest_attachemnt" {
