5579dbae94254c4b4a53fb289eaa82ab89c76182
Robert Miklas <robert.miklas@protonmail.com>
2022-09-16 00:32:57 +0200

    [aws_alb-1] deleted unused resources, fix in path resolution for 1 tg (#32)

--- providers/aws/alb-1/modules/alb_implementation/network.tf
+++ providers/aws/alb-1/modules/alb_implementation/network.tf
@@ -43,33 +43,6 @@ resource "aws_security_group" "traffic_to_ec2" {
   }
 }
 
-resource "aws_elb" "classic_lb" {
-  name               = "classic-lb"
-  availability_zones = [ var.zone1 ]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 5
-  }
-
-  instances                   = [aws_instance.root1.id, aws_instance.root2.id ]
-  cross_zone_load_balancing   = true
-  idle_timeout                = 400
-  connection_draining         = true
-  connection_draining_timeout = 400
-  //source_security_group_id = aws_security_group.lb_sg.id
-}
-
 data "aws_vpc" "default_vpc" {
   id = "vpc-0efe235925cc8fce0"
 }
